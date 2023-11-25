b23c8b49629972030b224a83ea8cc1ff7021980b
0xUgochukwu <0xUgochukwu@gmail.com>
2023-11-25 08:23:38 +0100

    fix: ai

--- ai.js
+++ ai.js
@@ -33,12 +33,12 @@ const sendMessage = async (messages) => {
 }
 
 async function digestMessage(r) {
-    if (typeof crypto < "u" && crypto?.subtle?.digest) {
+    // if (typeof crypto < "u" && crypto?.subtle?.digest) {
         const e = new TextEncoder().encode(r)
           , t = await crypto.subtle.digest("SHA-256", e);
         return Array.from(new Uint8Array(t)).map(a=>a.toString(16).padStart(2, "0")).join("")
-    } else
-        return crypto.createHash("sha256").update(r).digest("hex")
+    // } else
+        // return sha256Exports.sha256(r).toString()
 }
 
 const generateSignature = async (r) => {
