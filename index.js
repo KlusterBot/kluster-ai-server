525b2bdd5231af1a915298632585d0f73d41c32e
Dev Bash <ikorosamuel1@gmail.com>
2023-11-27 10:26:58 +0100

    Refactor code and add support for speaking to a human

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ I want you to reply with the solution, not write any explanations.
 Always redirect your conversations back to assisting visitors,
 Try not to talk much about other things unrelated to this.
 Your name is Kluster Bot,
+If i request to speak to a human,
+Reply with "{support}" on the first line of your response and a response below assuring me, an email has been sent to a human who will attend to me soon.
 In brackets below is our company information,
 {${model}}
 my first message is "${message}`;
@@ -37,10 +39,10 @@ my first message is "${message}`;
 const getAction = (message) => {
     const firstLine = message.split("\n")[0];
 
-    if (firstLine == "{call}") {
+    if (firstLine == "{support}") {
         return {
-            message: message.replace("{call}", "").trim(),
-            action: "call",
+            message: message.replace("{support}", "").trim(),
+            action: "support",
         };
     }
     return { message, action: "" };
@@ -55,39 +57,38 @@ app.get("/", (req, res) => {
 app.post("/", async (req, res) => {
     try {
         let { id, message, model } = req.body;
-    const messages = chatCache.get(id) || [];
+        const messages = chatCache.get(id) || [];
 
-    console.log({model})
+        console.log({ model });
 
-    if (messages.length === 0) {
-        messages.push({
-            role: "user",
-            content: getFirstMessage(message, model),
-        });
-    } else {
-        messages.push({ role: "user", content: message });
-    }
+        if (messages.length === 0) {
+            messages.push({
+                role: "user",
+                content: getFirstMessage(message, model),
+            });
+        } else {
+            messages.push({ role: "user", content: message });
+        }
 
-    console.log("Me:", message);
+        console.log("Me:", message);
 
-    const response = await ai(messages);
+        const response = await ai(messages);
 
-    if (!response) {
-        return res.send({}).status(500);
-    }
+        if (!response) {
+            return res.send({}).status(500);
+        }
 
-    messages.push({ role: "assistant", content: response });
+        messages.push({ role: "assistant", content: response });
 
-    chatCache.set(id, messages);
+        chatCache.set(id, messages);
 
-    console.log("AI:", response);
+        console.log("AI:", response);
 
-    const data = getAction(response);
+        const data = getAction(response);
 
-    res.send({ response: data.message, action: data.action });
-
+        res.send({ response: data.message, action: data.action });
     } catch (error) {
-        console.log(error)
+        console.log(error);
         res.send({}).status(500);
     }
 });
